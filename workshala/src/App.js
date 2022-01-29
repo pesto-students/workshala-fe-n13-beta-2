44d114f3b4ea375acd1be16f68e73bb5df7dac3f
suyashm002 <suyashm002@gmail.com>
2022-01-30 02:51:38 +0530

    applications page and jobs page completed

--- workshala/src/App.js
+++ workshala/src/App.js
@@ -2,23 +2,24 @@
 import * as React from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Pages/Home'
-import Dashboard from './Pages/Dashboard'
-import Error from './Pages/Error'
-import Profile from './Profile/Profile'
+import Home from "./Pages/Home";
+import Dashboard from "./Pages/Dashboard";
+import Error from "./Pages/Error";
+import Profile from "./Profile/Profile";
+import Jobs from "./Pages/Jobs";
+import Applications from "./Pages/Applications";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-        <Route index element={<Profile />} />
+      <Routes>
+        <Route index element={<Applications />} />
         <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/Home" element={<Home />} />
-        
+
         <Route path="*" element={<Error />} />
-      
-    </Routes>
-  </BrowserRouter>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
